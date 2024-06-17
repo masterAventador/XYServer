becce8d201386dad325e07d6be31da1c36e8eba9
masterAventador <419460185@qq.com>
2024-06-17 09:36:21 +0800

    change the release_build.sh ,not finished

--- release_build.sh
+++ release_build.sh
@@ -2,6 +2,7 @@
 build_script_path=build/Release/generators
 cmake_cache_path=build/Release
 bin_path=linux_bin
+server_address=root@8.140.207.230
 
 conan profile detect --name=ubuntu
 conan install . --build=missing -s build_type=Release --profile=ubuntu
@@ -13,4 +14,14 @@ cmake --build $cmake_cache_path
 
 source $build_script_path/deactivate_conanbuild.sh
 
-#nohup ./bin/XYServer > output.log 2>&1 &
+scp ./linux_bin/XYServer $server_address:/root/deploy/
+
+ssh $server_address
+
+cd deploy || echo "cd deploy error!!"
+
+pkill XYServer
+
+nohup ./XYServer > output.log 2>&1 &
+
+echo "Server start...!!!!"
